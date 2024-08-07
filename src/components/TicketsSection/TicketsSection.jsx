e71d354fb15f521518b6d2bac33d9d060ad1904f
Deen <inyoskillz@hotmail.com>
2024-08-07 15:53:57 +0300

    loading bar test

--- src/components/TicketsSection/TicketsSection.jsx
+++ src/components/TicketsSection/TicketsSection.jsx
@@ -1,15 +1,41 @@
 import ShowMoreTickets from '../ShowMoreTickets/ShowMoreTickets'
 import TicketSort from '../TicketSort/TicketSort'
 import TicketList from '../TicketList/TicketList'
+import { BiMessageError } from 'react-icons/bi'
 
 import styles from './TicketsSection.module.scss'
+import { selectLoading, selectStop, selectTickets } from '../../redux/slices/ticketsSlice'
+import { useSelector } from 'react-redux'
+import { selectFilters } from '../../redux/slices/filterSlice'
+import filterTickets from '../../utils/filterTickets'
+import { selectButtons } from '../../redux/slices/ticketSortSlice'
+
 
 function TicketsSection() {
+  const tickets = useSelector(selectTickets)
+  const filters = useSelector(selectFilters)
+  const activeButton = useSelector(selectButtons)
+  const stop = useSelector(selectStop)
+  const loading = useSelector(selectLoading)
+  const filteredTickets = filterTickets(tickets, filters, activeButton)
+
+
+
   return (
     <div className={styles.ticketsSection}>
       <TicketSort />
+
+      {!stop && <div className={styles.test}>fdsfd</div>}
+
       <TicketList />
-      <ShowMoreTickets />
+      {filteredTickets.length === 0 ? (
+        <div className={styles.error}>
+          <BiMessageError className={styles.errorIcon} />
+          <h4 className={styles.errorText}>Под заданные фильтры подходяших билетов не найдено</h4>
+        </div>
+      ) : (
+        <ShowMoreTickets />
+      )}
     </div>
   )
 }
