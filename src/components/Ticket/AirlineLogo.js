e9150245d72c024c0f745d21a9e435174d670e1b
Deen <inyoskillz@hotmail.com>
2024-08-08 16:54:09 +0300

    fixed airplane logos

--- src/components/Ticket/AirlineLogo.js
+++ src/components/Ticket/AirlineLogo.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 
 function AirlineLogo({ company }) {
   const logoUrl = `//pics.avs.io/99/36/${company}.png`
-  return <img src={logoUrl} alt="airline logo" />
+  return <img src={logoUrl} alt="airline logo" width={99} height={36} style={{ display: 'block' }} />
 }
 
 AirlineLogo.propTypes = {
